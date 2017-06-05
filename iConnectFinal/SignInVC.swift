64604adcdc896c39b355597167f03aa454333d64
Lingeswaran Kandasamy <lingeswarankandasamy@Lingeswarans-iMac.local>
2017-06-04 22:09:22 -0400

    completed facebook and firebase authentication final

--- iConnectFinal/SignInVC.swift
+++ iConnectFinal/SignInVC.swift
@@ -11,17 +11,20 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 import Firebase
 class SignInVC: UIViewController {
-
+    @IBOutlet weak var txtEmail: FancyTextFields!
+    @IBOutlet weak var txtPassword: FancyTextFields!
     override func viewDidLoad() {
         super.viewDidLoad()
+         self.hideKeyboardWhenTappedAround()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func facebookButtonTapped(_ sender: Any) {
         
         let facebookLogin = FBSDKLoginManager()
@@ -40,6 +43,7 @@ class SignInVC: UIViewController {
         
         
     }
+   
     func firebaseAuth(_ credentials: AuthCredential){
         Auth.auth().signIn(with: credentials, completion: { (user, Error)in
             if Error != nil {
@@ -50,5 +54,37 @@ class SignInVC: UIViewController {
             }
         })
     }
+    
+    @IBAction func signInBtnTapped(_ sender: Any) {
+        
+        if let email = txtEmail.text, let pwd = txtPassword.text {
+            
+            Auth.auth().signIn(withEmail: email, password: pwd) { (user, error) in
+                            if error == nil {
+                    print("Lin: EmailUser authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd) { (user, error) in
+                        if error != nil {
+                            print("Lin: unable to EmailUser authenticated with Firebase")
+                        } else {
+                            print("Lin: Authentication Success" )
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
 }
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
