6cd6cf7e39ed612abdb6cf45744dc266d78e813f
Lingeswaran Kandasamy <lingeswarankandasamy@Lingeswarans-iMac.local>
2017-06-14 00:38:17 -0400

    completed feed ui design

--- iConnectFinal/CircleView.swift
+++ iConnectFinal/CircleView.swift
@@ -1,23 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  iConnectFinal
 //
-//  Created by Lingeswaran Kandasamy on 5/28/17.
+//  Created by Lingeswaran Kandasamy on 6/13/17.
 //  Copyright © 2017 Lingeswaran Kandasamy. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6) .cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    
         
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
